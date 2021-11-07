dafaaeec58fe5d707f5d40955eb7c768d4058a01
pranavAgarkar7 <pranavagarkar7@gmail.com>
2021-11-07 20:48:26 +0530

    right commit

--- pname/settings.py
+++ pname/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y)w0ecoy=^k3%n1&i-!yn&ak#e9g$u9eqi^q=zh&tc0cjqyj(#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
